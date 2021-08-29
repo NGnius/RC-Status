f8c389c0f61ab46af1c517b7d2bc6b9f23107ab1
NGnius (Graham) <ngniusness@gmail.com>
2021-08-29 15:52:00 -0400

    Add CDN HTTP status check

--- src/cdn_status.rs
+++ src/cdn_status.rs
@@ -1,14 +1,21 @@
 use std::thread::{spawn, sleep, JoinHandle};
+use reqwest::blocking::ClientBuilder;
+use chrono::prelude::{Utc};
 
 use crate::ping::ping;
 
 const INDICATOR_NAME: &str = "CDN";
 
 pub fn start_worker() -> JoinHandle<()> {
-    spawn(payment_worker)
+    spawn(cdn_worker)
 }
 
-fn payment_worker() { // lol
+fn cdn_worker() { // lol
+    let mut sleep_dur = std::time::Duration::from_millis(crate::CONFIG.read().unwrap().period_ms);
+    let http_client = ClientBuilder::new()
+        .connect_timeout(sleep_dur)
+        .timeout(sleep_dur)
+        .build().expect("Failed to build auth worker HTTP client");
     while ! *crate::IS_STOPPING.read().unwrap() {
         let staticdata_ok = crate::CONTEXT.read().unwrap().staticdata_ok;
         if staticdata_ok {
@@ -16,13 +23,27 @@ fn payment_worker() { // lol
             let full_addr = crate::CONTEXT.read().unwrap().staticdata.AvatarCdnUrl.clone();
             let addr = full_addr.split("/").collect::<Vec<&str>>()[0];
             if let Ok(ping_time) = ping(addr) {
-                crate::CONTEXT.write().unwrap().indicators.update(INDICATOR_NAME, true, ping_time.avg, false);
+                let req = http_client.get(&full_addr);
+                let start = Utc::now();
+                let result = req.send();
+                let duration = ((Utc::now() - start).num_microseconds().unwrap() as f32)/1000.0;
+                if let Ok(resp) = result {
+                    if resp.status() == 404 || resp.status() == 200 {
+                        let avg_time = (duration + ping_time.avg) / 2.0;
+                        crate::CONTEXT.write().unwrap().indicators.update(INDICATOR_NAME, true, avg_time, true);
+                    } else {
+                        crate::CONTEXT.write().unwrap().indicators.update_error(INDICATOR_NAME, true, true);
+                    }
+                } else {
+                    crate::CONTEXT.write().unwrap().indicators.update_error(INDICATOR_NAME, true, true);
+                }
             } else {
                 crate::CONTEXT.write().unwrap().indicators.update_error(INDICATOR_NAME, true, false);
             }
         }
         // no API spam
         let dur = crate::CONFIG.read().unwrap().period_ms;
-        sleep(std::time::Duration::from_millis(dur));
+        sleep_dur = std::time::Duration::from_millis(dur);
+        sleep(sleep_dur);
     }
 }
