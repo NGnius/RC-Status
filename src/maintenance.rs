8b9cf979fb60db5f488273472af30c3e376cb496
NGnius (Graham) <ngniusness@gmail.com>
2021-11-26 10:03:49 -0500

    Re-enable (and fix) maintenance incidents

--- src/maintenance.rs
+++ src/maintenance.rs
@@ -1,6 +1,7 @@
 use std::thread::{spawn, sleep, JoinHandle};
 use crate::persist;
 
+#[allow(dead_code)]
 static DOMAIN: &str = "http://robocraftstaticdata.s3.amazonaws.com";
 
 pub fn start_worker() -> JoinHandle<()> {
@@ -8,45 +9,53 @@ pub fn start_worker() -> JoinHandle<()> {
 }
 
 fn maintenance_worker() {
-    return;
-    /*let mut old_sd: Option<crate::staticdata::StaticData> = None;
+    //return;
+    let mut old_sd: Option<crate::staticdata::StaticData> = None;
     while ! *crate::IS_STOPPING.read().unwrap() {
-        // TODO check if maintenance is starting/stopping
+        // check if maintenance is starting/stopping
+        let mut maintenace_msg: Option<persist::Incident> = None;
+        let mut maintenance_resolved = false;
         { // CONTEXT read lock scope (to not lock during sleep)
             let ctx = crate::CONTEXT.read().unwrap();
-            if old_sd.is_some() {
-                let some_old_sd = old_sd.clone().unwrap();
+            if let Some(some_old_sd) = old_sd.clone() {
+                //let some_old_sd = old_sd.clone().unwrap();
                 if ctx.staticdata_ok {
                     // check for maintenance
                     if some_old_sd.MaintenanceMode != ctx.staticdata.MaintenanceMode {
                         // maintenance mode has changed since last time
                         if ctx.staticdata.MaintenanceMode {
                             // entering maintenance mode
-                            persist::report(persist::Incident::Maintenance {
+                            maintenace_msg = Some(persist::Incident::Maintenance {
                                 time: persist::time_now(),
                                 resolved: persist::epoch(),
                                 message: ctx.staticdata.MaintenanceMessage.clone(),
                             });
                         } else {
                             // exiting maintenance mode
-                            persist::resolve(2, persist::time_now());
+                            maintenance_resolved = true;
                         }
                     }
                 } else {
                     // report error
-                    persist::report(persist::Incident::MiscOutage {
+                    // this leaks important info so it's disabled
+                    /*maintenace_msg = Some(persist::Incident::MiscOutage {
                         time: persist::time_now(),
                         resolved: persist::epoch(),
                         title: "Static Data Server Error".to_string(),
                         description: format!("This may prevent you from logging in. Error details: Error in response from {}", DOMAIN),
-                    });
+                    });*/
                 }
             }
             if ctx.staticdata_ok {
                 old_sd = Some(ctx.staticdata.clone());
             }
         }
+        if let Some(msg) = maintenace_msg {
+            persist::report(msg);
+        } else if maintenance_resolved {
+            persist::resolve(2, persist::time_now());
+        }
         // no API spam
         sleep(std::time::Duration::from_millis(crate::CONFIG.read().unwrap().period_ms));
-    }*/
+    }
 }
